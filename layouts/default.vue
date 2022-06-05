d4c9b6fababee86dacfaee598b5c24317d40b455
thepenielcho <mail@penielcho.com>
2022-06-06 00:28:34 +0900

    default body color to #b86297

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,6 +11,9 @@
 <style>
   @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css');
   * { font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif;; }
+  body {
+    background-color: #65B867;
+  }
 </style>
 
 <script>
